926d37a5d4052f626a54ce2e5b9ba5c9c495223b
Magnus Bergmark <magnus.bergmark@gmail.com>
2018-11-25 12:50:13 +0100

    Release version 1.0.0

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 # Build the latest version of the Docker image
 
-IMAGE_NAME=${IMAGE_NAME:-mange/prgnome:latest}
+version=$(
+  grep 'version =' Cargo.toml | head -n1 | awk '{ print $3 }' | tr -d '"'
+)
+image_name=mange/prgnome:${version}
+latest_name=mange/prgnome:latest
 
-docker build -t "$IMAGE_NAME" .
+echo -n "Will build and push ${image_name} and ${latest_name}. Press enter to continue."
+read -r
+
+docker build -t "$image_name" .
+docker tag "$image_name" "$latest_name"
+docker push "$image_name"
+docker push "$latest_name"
